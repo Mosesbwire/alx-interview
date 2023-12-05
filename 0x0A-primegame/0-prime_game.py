8f1e784bc5a80b996911458eeaac65f5da2412eb
Mosesbwire <mosesdev92@gmail.com>
2023-12-05 14:48:16 +0300

    Fix bug

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -14,39 +14,37 @@ def sqrt(n):
 
 def generate_primes(n: int) -> list[int]:
     """generate prime numbers between 0 and n"""
-    isPrime = [True for i in range(n + 1)]
-    isPrime[0] = False
-    isPrime[1] = False
+    if n < 2:
+        return []
+    is_prime = [True] * (n + 1)
+    is_prime[0] = False
+    is_prime[1] = False
 
-    for i in range(2, sqrt(n)):
-        if isPrime[i]:
+    for i in range(2, sqrt(n) + 1):
+        if is_prime[i]:
             j = i * i
             while j <= n:
-                isPrime[j] = False
+                is_prime[j] = False
                 j += i
-    primes = []
-
-    for i in range(len(isPrime)):
-        if isPrime[i]:
-            primes.append(i)
+    primes = [i for i in range(n + 1) if is_prime[i]]
     return primes
 
 
 def isWinner(x: int, nums: list[int]):
     """ return winner after x number of rounds played """
     score_board = {"miriam": 0, "ben": 0}
-    if x > len(nums):
+    if x > len(nums) or not nums:
         return None
-    primes = generate_primes(max(nums))
     for i in range(x):
+        primes = generate_primes(nums[i])
         if len(primes) == 0:
             score_board["ben"] = score_board["ben"] + 1
         elif len(primes) % 2 == 0:
             score_board["ben"] = score_board["ben"] + 1
         else:
             score_board["miriam"] = score_board["miriam"] + 1
-    if score_board["ben"] > score_board["miriam"]:
-        return "Ben"
-    if score_board["miriam"] > score_board["ben"]:
-        return "Miriam"
+    if score_board['ben'] > score_board['miriam']:
+        return 'Ben'
+    if score_board['miriam'] > score_board['ben']:
+        return 'Miriam'
     return None
