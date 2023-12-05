3fc5d6abdd4f8b69246c8897025ad4f983b03bbc
Mosesbwire <mosesdev92@gmail.com>
2023-12-05 13:00:39 +0300

    Complete prime game task

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+""" Prime game """
+
+
+def sqrt(n):
+    """returns square root of n"""
+    for i in range(1, n):
+        if i * i > n:
+            return i - 1
+        if i * i == n:
+            return i
+    return 0
+
+
+def generate_primes(n: int) -> list[int]:
+    """generate prime numbers between 0 and n"""
+    isPrime = [True for i in range(n + 1)]
+    isPrime[0] = False
+    isPrime[1] = False
+
+    for i in range(2, sqrt(n)):
+
+        if isPrime[i]:
+            j = i * i
+            while j <= n:
+                isPrime[j] = False
+                j += i
+    primes = []
+
+    for i in range(len(isPrime)):
+        if isPrime[i]:
+            primes.append(i)
+    return primes
+
+
+def isWinner(x: int, nums: list[int]):
+    score_board = {'miriam': 0, 'ben': 0}
+    if x > len(nums):
+        return None
+
+    for i in range(x):
+        primes = generate_primes(nums[i])
+        if len(primes) == 0:
+            score_board['ben'] = score_board['ben'] + 1
+        elif len(primes) % 2 == 0:
+            score_board['ben'] = score_board['ben'] + 1
+        else:
+            score_board['miriam'] = score_board['miriam'] + 1
+    if score_board['ben'] > score_board['miriam']:
+        return 'Ben'
+    if score_board['miriam'] > score_board['ben']:
+        return 'Miriam'
+    return None
+
+
+print(isWinner(5, [1, 2, 3, 4, 5]))
