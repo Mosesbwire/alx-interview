b75183e6a84bc8e2b1ebc29118809a3bd6992516
Mosesbwire <mosesdev92@gmail.com>
2023-12-20 11:44:40 +0300

    new file

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,52 +1,43 @@
 #!/usr/bin/python3
-'''0-prime_game
-    Alx-DSA interview question challenges
+'''module: prime GAME
 '''
 
 
-def sqrt(n):
-    """returns square root of n"""
-    for i in range(1, n):
-        if i * i > n:
-            return i - 1
-        if i * i == n:
-            return i
-    return 0
-
-
-def generate_primes(n: int) -> list[int]:
-    """generate prime numbers between 0 and n"""
-    if n < 2:
-        return []
-    is_prime = [True] * (n + 1)
-    is_prime[0] = False
-    is_prime[1] = False
-
-    for i in range(2, sqrt(n) + 1):
-        if is_prime[i]:
-            j = i * i
-            while j <= n:
-                is_prime[j] = False
-                j += i
-    primes = [i for i in range(n + 1) if is_prime[i]]
-    return primes
-
-
-def isWinner(x: int, nums: list[int]):
-    """return winner after x number of rounds played"""
-    score_board = {"miriam": 0, "ben": 0}
-    if x > len(nums) or not nums:
+def isWinner(x, nums):
+    '''prime game implementation
+    args
+        x is the number of rounds and nums is an array of n
+    Return:
+        name of the player that won the most rounds,
+        else return None
+    '''
+    if x is None or nums is None or x == 0 or nums == []:
         return None
+
+    mariaWins = 0
+    benWins = 0
+
     for i in range(x):
-        primes = generate_primes(nums[i])
-        if len(primes) == 0:
-            score_board["ben"] = score_board["ben"] + 1
-        elif len(primes) % 2 == 0:
-            score_board["ben"] = score_board["ben"] + 1
+        primes = get_prime(nums[i])
+        if len(primes) % 2 == 0:
+            benWins += 1
         else:
-            score_board["miriam"] = score_board["miriam"] + 1
-    if score_board["ben"] > score_board["miriam"]:
-        return "Ben"
-    if score_board["miriam"] > score_board["ben"]:
-        return "Miriam"
+            mariaWins += 1
+    if mariaWins > benWins:
+        return f'Maria'
+    elif benWins > mariaWins:
+        return f'Ben'
     return None
+
+
+def get_prime(n):
+    '''determine prime numbers:
+    '''
+    primes = []
+    filtered = [True] * (n + 1)
+    for prime in range(2, n + 1):
+        if (filtered[prime]):
+            primes.append(prime)
+            for i in range(prime, n + 1, prime):
+                filtered[i] = False
+    return primes
